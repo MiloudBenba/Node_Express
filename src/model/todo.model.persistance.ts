4a8b445749270d486691d1fa2eb0125a7e7f765c
Miloud Benbahlouli <miloudbenbahlouli@gmail.com>
2023-01-05 16:26:53 +0100

    docs: cours node_express Cyriak

--- src/model/todo.model.persistance.ts
+++ src/model/todo.model.persistance.ts
@@ -1,11 +1,23 @@
-export default class TodoPersistance {
-    id!: number
-    task: string
-    completed: boolean
+import { isKeyObject } from "util/types";
 
-    constructor(task: string ="") {
-        this.task = task
-        this.completed = false
+interface ITodo {
+    [task: string]: any
+}
+
+export default class TodoPersistance implements ITodo {
+    id!: number
+    task!: string
+    completed!: boolean
+
+    constructor(...data: any[]) {
+        const [obj] = data
+        for(let key in obj) {
+            console.log(key);
+            console.log(obj[key]);
+            // this[key] = obj[key]
+            
+            
+        }
     }
     
 }
